fd2207fca69f0a0cab014f1935746e9096299f19
tomo-1230 <satokawa1130tomoki2@gmail.com>
2026-07-20 14:07:33 +0900

    Move: laneEffect

---
+++ Assets/#Script/InGame/Effect/LaneTapEffect.cs
@@ -0,0 +1,28 @@
+using DG.Tweening;
+using UnityEngine;
+
+namespace InGame.Effect
+{
+    /// <summary>
+    /// ノーツヒット時のレーン演出
+    /// </summary>
+    public class LaneTapEffect : PoolObject
+    {
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+        [SerializeField] private Color _startColor;
+        [SerializeField] private float _endPosZ;
+        [SerializeField] private float _duration;
+
+        private void OnEnable()
+        {
+            _spriteRenderer.DOKill();
+            transform.DOKill();
+
+            _spriteRenderer.color = _startColor;
+            _spriteRenderer.DOFade(0, _duration);
+            this.transform.DOMoveZ(_endPosZ, _duration);
+
+            DOVirtual.DelayedCall(_duration, Release);
+        }
+    }
+}
