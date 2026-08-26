66ef50015006fbffea7ee82cd6e416b52cb19b53
tomo-1230 <satokawa1130tomoki2@gmail.com>
2026-08-26 17:35:28 +0900

    fix: SongSelctのScollViewズレ

--- Assets/#Script/Title/SongSelect/SongSelectGenerator.cs
+++ Assets/#Script/Title/SongSelect/SongSelectGenerator.cs
@@ -1,13 +1,16 @@
 using System.Collections.Generic;
 using Common.UI;
+using Cysharp.Threading.Tasks;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Title.SongSelect
 {
     public class SongSelectGenerator : ScrollViewBase
     {
         [SerializeField] private TextMeshProUGUI _titlePrefab;
+        [SerializeField] private RectTransform _layoutGroup;
         [SerializeField] private ContentsList _contentsListPrefab;
         [SerializeField] private SongUIControl _listSongPrefab;
         [SerializeField] private SongInfoControl _songInfoControl;
@@ -20,9 +23,10 @@ namespace Title.SongSelect
             DeleteChildren();
             AddContents("Ç®Ç∑Ç∑Çﬂ", SongRecommender.I.GetRecommendation());
             AddContents("ç≈ãﬂÇÃÉvÉåÉC", SongRecommender.I.GetPlayHistory());
+            ClonedActive();
         }
 
-        private void AddContents(string title , IReadOnlyList<SongSelectData> songSelectDatas)
+        private void AddContents(string title, IReadOnlyList<SongSelectData> songSelectDatas)
         {
             if (songSelectDatas == null || songSelectDatas.Count == 0) return;
 
@@ -35,11 +39,13 @@ namespace Title.SongSelect
         {
             DeleteChildren();
             AddContentsList(SongRecommender.I.GetKeywordSong(keyword));
+            ClonedActive();
         }
 
         public void UpdateSelect()
         {
             OnChangeSearchField(_keyword);
+            ClonedActive();
         }
 
         public void OnChangeSearchField(string text)
@@ -56,10 +62,19 @@ namespace Title.SongSelect
             _keyword = text;
         }
 
+        private void ClonedActive()
+        {
+            foreach (var obj in _clonedObject)
+                obj.gameObject.SetActive(true);
+
+            LayoutRebuilder.ForceRebuildLayoutImmediate(_layoutGroup);
+        }
+
         private void AddTitle(string text)
         {
             var title = InstantiateContent(_titlePrefab);
             title.text = text;
+            title.gameObject.SetActive(false);
             _clonedObject.Add(title.gameObject);
         }
 
@@ -67,6 +82,7 @@ namespace Title.SongSelect
         {
             var list = InstantiateContent(_contentsListPrefab);
             list.ViewList(datas, _listSongPrefab, OnSelect);
+            list.gameObject.SetActive(false);
             _clonedObject.Add(list.gameObject);
         }
 
