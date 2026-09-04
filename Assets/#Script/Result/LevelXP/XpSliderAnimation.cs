a3492aaa7342a69ff2ab60dc8843484ef38ae351
tomo-1230 <satokawa1130tomoki2@gmail.com>
2026-09-04 12:55:21 +0900

    fix: リザルトのプレイヤーXPバグ

--- Assets/#Script/Result/LevelXP/XpSliderAnimation.cs
+++ Assets/#Script/Result/LevelXP/XpSliderAnimation.cs
@@ -1,6 +1,6 @@
 using System;
 using DG.Tweening;
-using R3;
+using InGame.Score;
 using Title.PlayerData;
 using TMPro;
 using UnityEngine;
@@ -12,8 +12,10 @@ namespace Result.UI
     {
         [SerializeField] private LevelData _levelData;
         [SerializeField] private Image _slider;
+        [SerializeField] private float _waitDuration = 1f;
         [SerializeField] private float _duration = 0.5f;
         [SerializeField] private TextMeshProUGUI _sliderValueText;
+        [SerializeField] private TextMeshProUGUI _addXpValueText;
         [SerializeField] private TextMeshProUGUI _levelUpValueText;
         [SerializeField] private Ease _singleEase = Ease.InOutCubic;
         [SerializeField] private Ease _startEase = Ease.InOutCubic;
@@ -25,77 +27,108 @@ namespace Result.UI
 
         private void Awake()
         {
-            _slider.fillAmount = PlayerDataLoader.Info.XP / (float)_levelData.GetLevelUpXp(PlayerDataLoader.Info.Level);
+            _currentLevel = PlayerDataLoader.Info.Level;
+
+            float maxXP = _levelData.GetLevelUpXp(_currentLevel);
+            float fillAmount = PlayerDataLoader.Info.XP / maxXP;
+
+            _slider.fillAmount = fillAmount;
+            UpdateText(fillAmount);
         }
-        public void Play(IReadOnlyPlayerInfo startInfo,IReadOnlyPlayerInfo endInfo, Action<int> levelUpAction)
+
+        public void Play(IReadOnlyPlayerInfo startInfo, IReadOnlyPlayerInfo endInfo, Action<int> levelUpAction)
         {
-            int startLevel = startInfo.Level;
-            _currentLevel = startLevel;
-            int startXP = startInfo.XP;
-            int endLevel = endInfo.Level;
-            int endXP = endInfo.XP;
+            _addXpValueText.text = $"+{ScoreDataManager.ScoreData.GetXP()}XP";
+
             _sequence?.Kill();
 
-            _slider.fillAmount = startXP / (float)_levelData.GetLevelUpXp(_currentLevel);
+            int startLevel = startInfo.Level;
+            int endLevel = endInfo.Level;
+
+            _currentLevel = startLevel;
+
+            float startFill =
+                startInfo.XP /
+                (float)_levelData.GetLevelUpXp(startLevel);
+
+            _slider.fillAmount = startFill;
+            UpdateText(startFill);
 
             _sequence = DOTween.Sequence();
+            _sequence.AppendInterval(_waitDuration);
 
             // レベルアップなし
             if (startLevel == endLevel)
             {
-                _sequence.Append(DOMoveValue(endXP / (float)_levelData.GetLevelUpXp(_currentLevel), _duration, _singleEase));
+                float endFill = endInfo.XP / (float)_levelData.GetLevelUpXp(endLevel);
+                _sequence.Append(DOMoveValue(_slider.fillAmount, endFill, _duration, _singleEase));
 
                 _sequence.Play();
                 return;
             }
 
             // 最初のレベルの残りを埋める
-            _sequence.Append(DOMoveValue(1f, _duration, _startEase));
+            _sequence.Append(DOMoveValue(_slider.fillAmount, 1f, _duration, _startEase));
 
-            _sequence.AppendCallback(() =>
-            {
-                levelUpAction?.Invoke(startLevel + 1);
-                _slider.fillAmount = 0f;
-            });
+            // 最初のレベルアップ
+            _sequence.AppendCallback(GetLevelUpEvent(levelUpAction));
 
             // 中間レベル
             for (int level = startLevel + 1; level < endLevel; level++)
             {
-                _sequence.Append(DOMoveValue(1f, _duration, _fillEase));
-
-                int callbackLevel = level + 1;
-
-                _sequence.AppendCallback(() =>
-                {
-                    _currentLevel++;
-                    levelUpAction?.Invoke(callbackLevel);
-                    _slider.fillAmount = 0f;
-                });
+                _sequence.Append(DOMoveValue(0, 1f, _duration, _fillEase));
+                _sequence.AppendCallback(GetLevelUpEvent(levelUpAction));
             }
 
             // 最終レベル
-            _sequence.Append(DOMoveValue(endXP / (float)_levelData.GetLevelUpXp(endLevel), _duration, _endEase));
+            float endXP = endInfo.XP / (float)_levelData.GetLevelUpXp(endLevel);
+
+            _sequence.Append(DOMoveValue(0, endXP, _duration, _endEase));
 
             _sequence.Play();
         }
 
-        private Tween DOMoveValue(float value, float duration, Ease ease)
+        private TweenCallback GetLevelUpEvent(Action<int> levelUpAction)
         {
-            return DOVirtual.Float(_slider.fillAmount, value, duration, x =>
+            return () =>
             {
-                _slider.fillAmount = x;
-                UpdateText(x);
-            }).SetEase(ease);
+                _currentLevel++;
+
+                _slider.fillAmount = 0f;
+                UpdateText(0f);
+
+                levelUpAction?.Invoke(_currentLevel);
+            };
         }
+
+        private Tween DOMoveValue(float from, float value, float duration, Ease ease)
+        {
+            return DOVirtual.Float(from, value, duration,
+                x =>
+                {
+                    _slider.fillAmount = x;
+                    UpdateText(x);
+                })
+                .SetEase(ease);
+        }
+
         private void UpdateText(float value)
         {
-            int _LevelUpXp = _levelData.GetLevelUpXp(_currentLevel);
-            int currentXP = Mathf.RoundToInt(_LevelUpXp * value);
-            int remainXP = _LevelUpXp - currentXP;
+            int levelUpXP = _levelData.GetLevelUpXp(_currentLevel);
 
-            _sliderValueText.text = $"{currentXP:D5}/{_LevelUpXp:D5}";
+            int currentXP = Mathf.RoundToInt(levelUpXP * value);
+            int remainXP = Mathf.Max(0, levelUpXP - currentXP);
 
-            _levelUpValueText.text = $"次のレベルまで{remainXP:D5}";
+            _sliderValueText.text =
+                $"{currentXP:D5}/{levelUpXP:D5}";
+
+            _levelUpValueText.text =
+                $"次のレベルまで{remainXP:D5}";
+        }
+
+        private void OnDestroy()
+        {
+            _sequence?.Kill();
         }
     }
 }
